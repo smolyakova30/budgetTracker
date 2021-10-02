d04af928221fb081f9fca8fb93c0bbd71944bc6a
Olga <o.smolyakova30@gmail.com>
2021-10-02 17:38:17 -0500

    started to work on service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+// global constants 
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    
+];
+
+// install
+
+// activate 
+
+// get info
